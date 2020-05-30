d3959a03311698699ca90b496ad87cf7755022ce
finlaymcg <57777525+finlaymcg@users.noreply.github.com>
2020-05-30 17:56:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+window.addEventListener('load', () => {
+const sounds = document.querySelectorAll(".sound");
+const pads = document.querySelectorAll(".pads div");
+const visual = document.querySelector('.visual');
+const colors = [
+    "greenyellow",
+    "red",
+    "purple",
+    "yellow",
+    "rebeccapurple",
+    "peru"
+]
+
+
+
+//lets get going with the sound here.
+
+pads.forEach((pad, index) =>{
+    pad.addEventListener('click', function(){
+        sounds[index].currentTime = 0;
+        sounds[index].play();
+
+        createBubbles(index);
+    });
+});
+
+//Create a fucntion that makes some boobles
+const createBubbles = (index) => {
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index]
+bubble.style.animation = "jump 1s ease";
+bubble.addEventListener('animationend', function (){
+    visual.removeChild(this);
+})
+}
+});
+
